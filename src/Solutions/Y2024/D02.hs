27a37617f8d7bdae5e20168d7be90e7eb041d4a8
tobeannouncd <wzrds3@gmail.com>
2024-12-02 11:17:16 -0500

    Added `$O(n)$` solution to day 2 2024

--- src/Solutions/Y2024/D02.hs
+++ src/Solutions/Y2024/D02.hs
@@ -7,6 +7,8 @@ main = do
   xss <- map (map read . words) . lines <$> input
   answer $ length $ filter (isSafeWith pure) xss
   answer $ length $ filter (isSafeWith holes) xss
+  -- answer $ length $ filter (isGood False) xss
+  -- answer $ length $ filter (isGood True) xss
 
 isSafeWith :: ([Int] -> [[Int]]) -> [Int] -> Bool
 isSafeWith f = any isSafe . f
@@ -21,3 +23,19 @@ isSafe xs =
       | y > 0 -> all (\x -> 1 <= x && x <= 3) ys
       | y < 0 -> all (\x -> -3 <= x && x <= -1) ys
     _ -> False
+
+-- | @\(O(n)\)@ solution. Not really necessary due to the size of the given
+--   inputs.
+-- isGood :: Bool -> [Int] -> Bool
+-- isGood p xs = good p xs || good p (reverse xs)
+
+-- good :: Bool -> [Int] -> Bool
+-- good p = go []
+--   where
+--     go _ [] = True
+--     go [] (x:xs) = go [x] xs
+--     go xs@(x:_) (y:ys)
+--       | d <- x - y
+--       , 1 <= d, d <= 3 = go (y:xs) ys
+--     go [x] ys = p && any (good False) [ys, x:drop 1 ys]
+--     go (x:x':_) ys = p && any (good False) [x':ys, x:drop 1 ys]
