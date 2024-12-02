22e8248695f902a0e0eb5c004cb6d7a66611a9a3
tobeannouncd <wzrds3@gmail.com>
2024-12-02 01:03:57 -0500

    refactor day 2 2024

--- src/Solutions/Y2024/D02.hs
+++ src/Solutions/Y2024/D02.hs
@@ -9,13 +9,15 @@ main = do
   answer $ length $ filter (isSafeWith holes) xss
 
 isSafeWith :: ([Int] -> [[Int]]) -> [Int] -> Bool
-isSafeWith f = any (isSafe . (zipWith (-) <*> tail)) . f
+isSafeWith f = any isSafe . f
 
 holes :: [a] -> [[a]]
 holes xs = xs : [i ++ t | (i, _:t) <- zip (inits xs) (tails xs)]
 
 isSafe :: (Ord a, Num a) => [a] -> Bool
-isSafe xs@(x:_)
-  | x < 0 = all (\y -> -3 <= y && y <= -1) xs
-  | x > 0 = all (\y -> 1 <= y && y <= 3) xs
-isSafe _ = False
+isSafe xs =
+  case zipWith (-) xs (tail xs) of
+    ys@(y:_)
+      | y > 0 -> all (\x -> 1 <= x && x <= 3) ys
+      | y < 0 -> all (\x -> -3 <= x && x <= -1) ys
+    _ -> False
